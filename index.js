72d072e5441d06d9a069fbd6d4f3ff8b13d105c7
ckwahome <ckwahome@gmail.com>
2025-09-28 07:31:46 +0000

    weather app

--- index.js
+++ index.js
@@ -1,27 +1,80 @@
-let usernameInput = document.getElementById('usernameInput');
-let passwordInput = document.getElementById('passwordInput');
-let show = document.getElementById('show');
-let btn=document.querySelector('button')
-let p= document.getElementById('greet');
-
-show.onclick=function(){
+const apiKey='6d4a2742422c62bd0eb8eaf999784c63'
 
 
-    if(passwordInput.type === "password"){
-        passwordInput.type = "text";
-        show.innerText = "hide";
-        
+// API details
+const API_KEY = apiKey; // Get free from openweathermap.org
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+
+const input = document.getElementById("cityInput");
+const button = document.getElementById("Btn");
+const result = document.getElementById("results");
+
+/*// fetch weather data
+async function getWeather(city) {
+  try {
+    // Build API URL
+    const url = `${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`;
+
+    // Fetch data from API
+    const response = await fetch(url); // fetch returns a promise
+    if (!response.ok) {
+      throw new Error("City not found");
     }
 
+    // Convert response to JSON
+    const data = await response.json();
+    
+    
 
-    //if input type is text
-    else{
-        passwordInput.type = "password";
-        show.innerText = "show";
-        show.cursor = "pointer";
+    // Extract useful info
+    const temperature = data.main.temp;
+    const description = data.weather[0].description;
+
+    // Display result
+    result.innerHTML = `
+      <h3>Weather in ${city}</h3>
+      <p>Temperature: ${temperature}℃</p>
+      <p>Condition: ${description}</p>
+    `;
+  } catch (error) {
+    // Handle errors (e.g., wrong city name)
+    result.innerHTML = `<p style="color:red;">${error.message}</p>`;
+  }
+}
+
+// Add click event to button
+button.addEventListener("click", () => {
+  const city = input.value.trim();
+     
+    console.log(city);
+  if (city) {
+    getWeather(city); // call async function
+  } else {
+    result.innerHTML = "<p style='color:red;'>Please enter a city name!</p>";
+  }
+});
+
+
+*/
+
+
+
+
+
+
+
+/*let worked= false
+function add(num,err){
+    if(worked){
+         console.log(num);
     }
+   else{
+        console.log(err);
+   }
+    
+    
+    
 }
-
-btn.onclick=function(){
-p.innerText = `hello ${usernameInput.value} welcome to our website`;
-}
+add("success",'failed')
+*/
+    
